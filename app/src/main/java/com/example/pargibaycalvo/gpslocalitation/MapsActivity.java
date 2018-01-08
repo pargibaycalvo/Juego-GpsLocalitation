32d28274ea2443b1d285a6ed8728580933845ffc
Pedro José Argibay Calvo <pdrjs616@gmail.com>
2018-01-08 12:47:16 +0100

    Añadido imagenes de puntero y localizaciones de las pistas 3

--- app/src/main/java/com/example/pargibaycalvo/gpslocalitation/MapsActivity.java
+++ app/src/main/java/com/example/pargibaycalvo/gpslocalitation/MapsActivity.java
@@ -27,22 +27,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -59,9 +48,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Mostrar diálogo explicativo
             } else {
-                // Solicitar permiso
+                // Solicitar permisos de usuario
                 ActivityCompat.requestPermissions(
                         this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
@@ -71,7 +59,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
         //mMap.setMyLocationEnabled(true);
-        // Marcadores
+
+        // Marcadores 3 pistas
         mMap.addMarker(new MarkerOptions().position(new LatLng(42.23661386151706, -8.714480996131897)));
 
         LatLng latLng = new LatLng(42.237439526686515, -8.714226186275482);//La Fayette
@@ -82,7 +71,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .snippet("Fundador: pargibay 6150")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.pandariam)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
 
         LatLng latLng1 = new LatLng(42.237706320945556, -8.715687990188599);//GaliPizza
 
