133981a6566721d43e3bbbbfaf1429bb946607b0
Pedro José Argibay Calvo <pdrjs616@gmail.com>
2018-01-22 11:39:20 +0100

    añadido QR

--- app/src/main/java/com/example/pargibaycalvo/gpslocalitation/SimpleScannerActivity.java
+++ app/src/main/java/com/example/pargibaycalvo/gpslocalitation/SimpleScannerActivity.java
@@ -4,5 +4,68 @@ package com.example.pargibaycalvo.gpslocalitation;
  * Created by pargibaycalvo on 22/1/18.
  */
 
-public class SimpleScannerActivity {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class SimpleScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+    static final String TAG = "escaneoQR";
+
+    private static final int MY_PERMISSIONS = 1 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        Log.v(TAG, "Inicializado scan");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS);
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v(TAG, rawResult.getText());
+        Log.v(TAG, rawResult.getBarcodeFormat().toString());
+
+        mScannerView.resumeCameraPreview(this);
+        Intent intento= new Intent();
+        intento.putExtra("retorno", rawResult.getText() );
+        setResult(RESULT_OK, intento);
+        finish();
+
+    }
 }
